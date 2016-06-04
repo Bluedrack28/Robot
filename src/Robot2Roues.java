4c634816eb93b5156416789555d4491957a87df0
Bluedrack28 <Bluedrack28@gmail.com>
2016-06-04 16:17:12 +0200

    Simplification et nettoyage

--- src/Robot2Roues.java
+++ src/Robot2Roues.java
@@ -1,22 +1,19 @@
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Timer;
-import java.util.TimerTask;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
-
 public class Robot2Roues {
 
 	private EV3LargeRegulatedMotor roueGauche;
 	private EV3LargeRegulatedMotor roueDroite;
+
+	public int NumberClarck;
 	private static double Lumiere;
 	private static double Correction;
 	private static double VitesseB;
@@ -25,26 +22,13 @@ public class Robot2Roues {
 
 	/**
 	 * 
-	 * @param portRoueGauche
-	 *            MotorPort.(Lettre du port)
-	 * @param portRoueDroite
-	 *            MotorPort.(Lettre du port)
+	 * @param portRoueGauche Port moteur gauche 
+	 * @param portRoueDroite Port moteur droit
 	 */
 	public Robot2Roues(Port portRoueGauche, Port portRoueDroite) {
 		roueGauche = new EV3LargeRegulatedMotor(portRoueGauche);
 		roueDroite = new EV3LargeRegulatedMotor(portRoueDroite);
 	}
-
-	/*
-	 * public static EV3LargeRegulatedMotor MotorA = new
-	 * EV3LargeRegulatedMotor(MotorPort.A); public static EV3LargeRegulatedMotor
-	 * roueDroite = new EV3LargeRegulatedMotor(MotorPort.B); public static
-	 * EV3LargeRegulatedMotor roueGauche = new
-	 * EV3LargeRegulatedMotor(MotorPort.C); public static EV3LargeRegulatedMotor
-	 * MotorD = new EV3LargeRegulatedMotor(MotorPort.D);
-	 */
-
-	public int NumberClarck;
 
 	public void rotateWaitAColour(int color, int time, int vitesse) {
 
@@ -107,15 +91,17 @@ public class Robot2Roues {
 	}
 
 	public void followTheLineWait(Port capteurCouleur, double moyenCouleur, int timerMs) {
+
 		SampleProvider color = new EV3ColorSensor(capteurCouleur).getRedMode();
 		float[] sample = new float[color.sampleSize()];
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		while(cal.compareTo(Calendar.getInstance())*-1>= timerMs){
-			
-	        LCD.drawString( sdf.format(cal.getTime()), 1, 1);
-	        Delay.msDelay(1000);
-	        color.fetchSample(sample, 0);
+
+		while (cal.compareTo(Calendar.getInstance()) * -1 >= timerMs) {
+
+			LCD.drawString(sdf.format(cal.getTime()), 1, 1);
+			Delay.msDelay(1000);
+			color.fetchSample(sample, 0);
 			Lumiere = sample[0];
 
 			Correction = (Lumiere - moyenCouleur) * 0.4;
@@ -131,32 +117,10 @@ public class Robot2Roues {
 			Delay.msDelay(10);
 			roueDroite.forward();
 			roueGauche.forward();
-	        
 
 		}
-		/*
-				
 		
-		while (true) {
 
-			color.fetchSample(sample, 0);
-			Lumiere = sample[0];
-
-			Correction = (Lumiere - 0.325) * 0.4;
-			VitesseC = ((Correction * -1) + 0.4) * roueDroite.getMaxSpeed();
-			VitesseB = ((Correction * 1) + 0.4) * roueGauche.getMaxSpeed();
-
-			roueDroite.setSpeed((float) VitesseC);
-			LCD.drawString(Double.toString(VitesseB), 1, 1);
-			LCD.drawString(Double.toString(VitesseC), 1, 2);
-			LCD.drawString(Double.toString(Lumiere), 1, 3);
-
-			roueGauche.setSpeed((float) VitesseB);
-			Delay.msDelay(10);
-			roueDroite.forward();
-			roueGauche.forward();
-		}
-		*/
 	}
 
 }
